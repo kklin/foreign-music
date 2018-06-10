aee0e24c91da902ee5bd23e0f947b95fa40a0afd
Kevin Lin <kevin@kelda.io>
2018-06-09 21:17:19 -0700

    recommendations: Seed database with playlist information

---
+++ recommendations/recommendations.js
@@ -0,0 +1,107 @@
+const SpotifyWebApi = require('spotify-web-api-node');
+const PostgresClient = require('pg').Client;
+
+const genres = ['edm', 'reggae', 'pop', 'rock', 'folk', 'jazz', 'punk', 'metal', 'blues', 'funk', 'disco', 'rap', 'acoustic', 'grunge', 'jazz'].slice(0, 5);
+const nationalities = ['chinese', 'indian', 'indonesian', 'brazilian', 'pakistani', 'nigerian', 'russian', 'japanese', 'mexican', 'ethiopian', 'filipino', 'egyptian', 'vietnamese', 'iranian', 'turkish', 'german', 'italian', 'french'].slice(0, 5);
+
+const pgClient = new PostgresClient({
+  user: 'foreign_music',
+  host: 'foreign-music.c0prpxq16lno.us-east-2.redshift.amazonaws.com',
+  database: 'prod',
+  password: 'i4X$dxzeGBAp',
+  port: 5439,
+});
+pgClient.connect().then(() => {
+  const spotifyApi = new SpotifyWebApi({
+    clientId: '33698d56449e4a8c9226f27573756d16',
+    clientSecret: 'b9daa0497ddf4aeba11fba6a1479c8c9',
+  });
+
+  // Retrieve an access token.
+  // TODO: This will probably be switched to Authorization
+  // Code Flow from Client Credential Flow once user authentication is needed
+  // (for playing music, and saving music to their library).
+  spotifyApi.clientCredentialsGrant().then(
+    (data) => {
+      // Save the access token so that it's used in future calls
+      spotifyApi.setAccessToken(data.body['access_token']);
+    },
+    (err) => {
+      console.log('Something went wrong when retrieving an access token', err);
+    }
+  ).then(() => {
+    //spotifyApi.searchArtists('Daniel Caesar').then((resp) => {
+    //console.log(resp.body.artists.items[0]);
+    //}, console.log);
+
+    const promises = [];
+    genres.forEach((genre) => {
+      nationalities.forEach((nationality) => {
+        // TODO: Use maximum pagination.
+        const promise = spotifyApi.searchPlaylists(`${nationality} ${genre}`).then((resp) => {
+          // Get 5 random playlists.
+          const playlistSample = getRandomItems(resp.body.playlists.items, 5);
+
+          return Promise.all(playlistSample.map((playlist) => {
+            return pgClient.query('INSERT INTO playlists(id, owner_id, name, genre, country) VALUES ($1, $2, $3, $4, $5)',
+              [playlist.id, playlist.owner.id, playlist.name.slice(0,128), genre, nationality]).then(() => {
+                console.log('inserted');
+              });
+          }));
+
+          // For each playlist, get 10 random tracks.
+          // TODO: Use maximum pagination.
+          //playlistSample.forEach((playlist) => {
+            //spotifyApi.getPlaylistTracks(playlist.owner.id, playlist.id).then((resp) => {
+              // Save track information to database.
+              //const trackSample = getRandomItems(resp.body.items, 10);
+              //console.log(trackSample);
+            //});
+          //});
+
+          // Get the artist information for each track. Use a cache.
+          // Save the artist information. Their name, ID, popularity, occurences in search, genres.
+        }, console.log);
+        promises.push(promise);
+      });
+    });
+
+    Promise.all(promises).then(() => {
+      pgClient.end().then(() => console.log('closed'));
+    });
+
+    //spotifyApi.searchTracks('Best Part (feat H.E.R.)').then((resp) => {
+    //console.log(resp.body.tracks.items);
+    //}, console.log);
+
+    //spotifyApi.getRecommendations({
+    //seed_artists: '7Dx7RhX0mFuXhCOUgB01uM', // JJ Lin.
+    //seed_artists: '1dVygo6tRFXC8CSWURQJq2', // Sonu Nigam.
+    //seed_tracks: '1RMJOxR6GRPsBHL8qeC2ux', // Best part
+    //}).then((resp) => {
+    //console.log('Recommended tracks');
+    //resp.body.tracks.forEach((track) => {
+    //const artists = spotifyApi.getArtist(track.artists[0].id).then((artist) => {
+    //console.log(`${track.artists[0].name} - ${track.name} (${artist.body.genres})`);
+    //})
+    //});
+    //}, console.log);
+  });
+}, console.error);
+
+function getRandomItems(arr, n) {
+  if (arr.length < n) {
+    return arr;
+  }
+
+  const randomIndices = new Set();
+  while (randomIndices.size != n) {
+    randomIndices.add(getRandomNumber(n));
+  }
+
+  return Array.from(randomIndices).map((idx) => arr[idx]);
+}
+
+function getRandomNumber(n) {
+  return Math.floor(Math.random() * n);
+}
